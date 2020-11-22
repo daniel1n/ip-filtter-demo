26049565e30fc9e41c4fd7c61df22e486912d775
daniel1n <492332435@qq.com>
2020-11-22 17:27:31 +0800

    first commit

---
+++ src/main/java/com/qqlin/ipFilterDemo/IpFilterDemoApplication.java
@@ -0,0 +1,14 @@
+package com.qqlin.ipFilterDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class IpFilterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpFilterDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qqlin/ipFilterDemo/IpFiltterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqlin.ipFilterDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpFiltterDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
